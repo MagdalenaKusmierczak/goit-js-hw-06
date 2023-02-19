125d77384f7a149824de58890afc341ad8d1ee32
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-02-19 20:13:39 +0100

    task 4 finish

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+"use strict";
+let counterValue = 0;
+const displayCounterValue = document.querySelector("#value");
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+
+function addingValue() {
+  counterValue = counterValue + 1;
+  displayCounterValue.textContent = counterValue;
+}
+function subtractingValue() {
+  counterValue = counterValue - 1;
+  displayCounterValue.textContent = counterValue;
+}
+
+incrementBtn.addEventListener("click", addingValue);
+decrementBtn.addEventListener("click", subtractingValue);
