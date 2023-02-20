b745bbb6c3a5834d1bbf399eb2c3a2947d7f5d84
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-02-20 19:08:33 +0100

    task 8 finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+"use strict";
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const formDetails = { email: email.value, password: password.value };
+
+  console.log(formDetails);
+  event.currentTarget.reset();
+}
+
+// Opracowanie przesyłania formularza form.login-form powinno przebiegać zgodnie ze zdarzeniem submit.
+// Podczas przesyłania formularza strona nie powinna się restartować.
+// Jeśli w formularzu są nieuzupełnione pola, wprowadź alert z upomnieniem o tym, że wszystkie pola powinny zostać wypełnione.
+// Jeśli użytkownik uzupełnił wszystkie pola i wysłał formularz, zbierz wartość pól jako obiekt, gdzie nazwa pola będzie nazwą właściwości, a wartość pola - wartością właściwości. Aby otrzymać dostęp do elementów formularza użyj właściwości elements.
+// Umieść obiekt z wprowadzonymi danymi do wiersza poleceń i wyczyść wartości pól formularza metodą reset.
