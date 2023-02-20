03fdf30520b89f6bb03acc3e85abf5f915c5dff1
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-02-20 20:08:17 +0100

    task 10 finish

--- js/task-10.js
+++ js/task-10.js
@@ -1,10 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const inputValue = document.querySelector("#conrols>input");
+const inputValue = document.querySelector("#controls>input");
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 const boxes = document.querySelector("#boxes");
+console.log(inputValue);
+
+function createBoxes(amount) {
+  const newElements = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const boxSize = `${30 + 10 * i}px`;
+    box.style.height = boxSize;
+    box.style.width = boxSize;
+    box.style.background = getRandomHexColor();
+    newElements.push(box);
+  }
+  return newElements;
+}
+
+createBtn.addEventListener("click", () => {
+  let newBoxes = createBoxes(inputValue.value);
+  boxes.append(...newBoxes);
+});
+destroyBtn.addEventListener("click", () => {
+  inputValue.value = "";
+  boxes.innerHTML = "";
+});
 
 // Napisz skrypt tworzenia i usuwania kolekcji elementów.
 // Użytkownik wprowadza liczbę elementów do input i naciska
