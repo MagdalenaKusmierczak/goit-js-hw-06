9f084bf4a6d37628850a9ea1c7d3c963846369f4
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-02-20 14:43:11 +0100

    task 7 finish

---
+++ js/task-07.js
@@ -0,0 +1,13 @@
+"use strict";
+const rangeInput = document.querySelector("#font-size-control");
+const outputText = document.querySelector("#text");
+
+rangeInput.addEventListener("change", () => {
+  const rangeValue = rangeInput.value;
+  outputText.style.fontSize = `${rangeValue}px`;
+});
+
+// Napisz skrypt, który reaguje na zmianę wartości input#font - size - control
+//     (zdarzenie input) i zmienia styl inline span#text aktualizując właściwość
+// font - size.W rezultacie, podczas przeciągania paska przesuwania będzie
+// zmieniał się rozmiar tekstu.
