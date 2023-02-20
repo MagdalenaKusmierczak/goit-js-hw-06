d7749f8d54c95f4eb5b834cfa95482e5e54a78b7
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-02-20 19:30:30 +0100

    task 9 finish

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const spanText = document.querySelector(".color");
+const changeBtn = document.querySelector(".change-color");
+
+const handleClick = () => {
+  spanText.textContent = getRandomHexColor();
+  body.style.backgroundColor = `${spanText.textContent}`;
+};
+
+changeBtn.addEventListener("click", handleClick);
+
+// Napisz skrypt, który zmienia kolor tła elementu < body >
+//   poprzez style inline po kliknięciu na button.change -
+//     color i wprowadza wartość koloru do span.color.
+// Aby wygenerować losowy kolor użyj funkcji getRandomHexColor.
