65ea6b3ed4a5569e829979412da17bb91ad64350
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-02-21 19:38:19 +0100

    task 1 differnt direction

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,14 @@ const listItems = document.querySelectorAll(".item");
 const numberOfItems = listItems.length;
 console.log(`Number of categories: ${numberOfItems}`);
 
+// listItems.forEach((element) => {
+//   console.log(`Category: ${element.firstElementChild.textContent}`);
+//   console.log(`Elements: ${element.lastElementChild.children.length}`);
+// });
+
 listItems.forEach((element) => {
-  console.log(`Category: ${element.firstElementChild.textContent}`);
-  console.log(`Elements: ${element.lastElementChild.children.length}`);
+  const header = element.querySelector("h2");
+  const listElements = element.querySelectorAll("li");
+  console.log(`Category: ${header.textContent}`);
+  console.log(`Elements: ${listElements.length}`);
 });
